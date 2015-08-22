a13cfc0dc272e8c25141866cd76778cf8938590a
Eamonn OToole <otooleea@scss.tcd.ie>
2015-08-22 14:08:14 +0100

    Pushing final script and Report

--- trainAndTest.R
+++ trainAndTest.R
@@ -1,5 +1,9 @@
+# Step 1: Load Libraries
 library(caret)
+library(doParallel)
+registerDoParallel(cores=2)
 
+# Step 2 - Get and load the data
 checkAndDownloadFiles <- function(){
   ## First check if data folder exists
   if (!dir.exists("data")){
@@ -15,13 +19,17 @@ checkAndDownloadFiles <- function(){
   }
 }
 
-readInData <- function(){
+readInTrainingData <- function(){
   filename <- "data/pml-training.csv"
   training <- read.table(filename, sep=",", skip=0, header=TRUE, 
                          na.strings=c("NA","NaN",""," ","#DIV/0!"))
   return (training)
 }
 
+checkAndDownloadFiles()
+allTrainingData <- readInTrainingData() 
+
+# Step 3 - Partition training & test datasets
 createTrainTest <- function(theData){
   set.seed(41282)
   inTrain <- createDataPartition(y = theData$classe, p=.70, list=FALSE)
@@ -30,71 +38,52 @@ createTrainTest <- function(theData){
   return (list(training = training, testing = testing))
 }
 
-  
-  # Step 1 - Check for and download raw data if necessary
-  checkAndDownloadFiles()
-  
-  # Step 2 - Read in the training data
-  allData <- readInData() 
-  
-  # Step 3 - create the training & test datasets
-  datasets <- createTrainTest(allData)
-  
-  # Step 4 - Process the data by removing poor features
-  training <- datasets$training
-    # A. Remove columns that are mostly NA
-  training <- training[,colSums(is.na(training)) < nrow(training) * 0.5]
-    # B. Next Remove columsn relating to Index, timestamps and windows
-  training <- training[,-(1:7)]
-    # C. Check for highly correlated features and remove unnecessary features
-  descrCor <- cor(training[,-ncol(training)])
-  highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
-  training <- training[,-highlyCorDescr]
+datasets <- createTrainTest(allTrainingData)
+training <- datasets$training
+# Step 4 - Process the data 
+# A. Remove columns that are mostly NA
+training <- training[,colSums(is.na(training)) < nrow(training) * 0.5]
+any(is.na(training))
+# B. Next Remove columsn relating to Index, timestamps and windows
+training <- training[,-(1:7)]
+# C. Check for highly correlated features and remove unnecessary features
+descrCor <- cor(training[,-ncol(training)])
+highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
+training <- training[,-highlyCorDescr]
   
   
-  # Step 5 - Train the model
-  lastCol <- ncol(training)
-  preProc.Norm <- preProcess(training[,-lastCol],method=c("center","scale"))
+# Step 5 - Training
+lastCol <- ncol(training)
+preProc.Norm <- preProcess(training[,-lastCol],method=c("center","scale"))
   
-  # Fit the models
-  train.Norm <- predict(preProc.Norm, training[,-lastCol])
+train.Norm <- predict(preProc.Norm, training[,-lastCol])
   
-  set.seed(360)
-  if(!file.exists("model_Norm.Rds")){
-    print("Model file for Center+Scale not found. About to train new model. 
+set.seed(360)
+if(!file.exists("model_Norm.Rds")){
+  print("Model file for Center+Scale not found. About to train new model. 
           Go get a coffee, this will take a while")
-    library(doParallel)
-    registerDoParallel(cores=2)
-    modelFit.Norm <- train(training$classe ~., method="rf" ,data=train.Norm,
-                           trControl=trainControl(method="cv",number=5),
-                           prox=TRUE,allowParallel=TRUE)
-    saveRDS(modelFit.Norm, "model_Norm.Rds")
-    registerDoParallel(cores=1)
-  }else{
-    print("Loading Ceneter+Scale model")
-    modelFit.Norm <- readRDS("model_Norm.Rds") 
-  }
+  modelFit.Norm <- train(training$classe ~., method="rf" ,data=train.Norm,
+                      trControl=trainControl(method="cv",number=5),
+                      prox=TRUE,allowParallel=TRUE)
+  saveRDS(modelFit.Norm, "model_Norm.Rds")
+  registerDoParallel(cores=1)
+}else{
+  print("Loading Ceneter+Scale model")
+  modelFit.Norm <- readRDS("model_Norm.Rds") 
+}
   
-  print("The Model")
-  modelFit.Norm
-  print("FinalModel")
-  modelFit.Norm$finalModel
   
-  cmTrain <- confusionMatrix(training$classe,predict(modelFit.Norm,train.Norm))
-  print(cmTrain$overall)
-  print(cmTrain$table)
-  
-  # Step 6 - Evaluate on the test set
-  testing <- datasets$testing
-  testing <- testing[,names(training)]
-  test.Norm <- predict(preProc.Norm, testing[,-lastCol])
-  preds <- predict(modelFit.Norm, test.Norm)
-  testing$predRight <- preds == testing$classe
-  table(preds, testing$classe)
-  
-  cmTest <- confusionMatrix(testing$classe,predict(modelFit.Norm,test.Norm))
-  print(cmTest$overall)
-  print(cmTest$table)
-  print(nrow(testing[testing$predRight == TRUE,]) / nrow(testing))
+print("FinalModel")
+print(modelFit.Norm$finalModel)
   
 
+# Step 6 - Apply to test partition
+testing <- datasets$testing
+testing <- testing[,names(training)]
+test.Norm <- predict(preProc.Norm, testing[,-lastCol])
+preds <- predict(modelFit.Norm, test.Norm)
+testing$predRight <- preds == testing$classe
+
+table(preds, testing$classe)
+
+print(nrow(testing[testing$predRight == TRUE,]) / nrow(testing))
